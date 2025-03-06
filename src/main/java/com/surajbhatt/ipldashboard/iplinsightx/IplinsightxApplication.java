5e15aeda7fde54b1e0c07bbb1c6a1a0ab532ef31
Suraj Bhatt <surajbhatt54321@gmail.com>
2025-03-06 19:28:33 +0530

    stub up a spring boot backend for IPLInsightX

---
+++ src/main/java/com/surajbhatt/ipldashboard/iplinsightx/IplinsightxApplication.java
@@ -0,0 +1,13 @@
+package com.surajbhatt.ipldashboard.iplinsightx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplinsightxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplinsightxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/surajbhatt/ipldashboard/iplinsightx/IplinsightxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surajbhatt.ipldashboard.iplinsightx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplinsightxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
